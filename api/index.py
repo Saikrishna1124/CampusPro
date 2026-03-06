b02c5c64f2d5320fc04fafe5acd1c428cb291d3e
Sai Krishna Gummadidala <saikrishnagummadidala34@gmail.com>
2026-03-06 16:07:57 +0530

    Fix Vercel deployment

--- api/index.py
+++ api/index.py
@@ -2,21 +2,17 @@ import os
 import sys
 from pathlib import Path
 
-# Add project root to path
 BASE_DIR = Path(__file__).resolve().parent.parent
 sys.path.append(str(BASE_DIR))
 
-# Set Django settings
 os.environ.setdefault(
     "DJANGO_SETTINGS_MODULE",
     "student_management_project.settings"
 )
 
-# Initialize Django
 import django
 django.setup()
 
-# Get the WSGI application
 from django.core.wsgi import get_wsgi_application
 
 app = get_wsgi_application()
